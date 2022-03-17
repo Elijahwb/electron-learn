6160f3dcc92f4c989f9c220cd4635e53c85f6496
Brandon Elijah <brandonelijah099@gmail.com>
2022-03-17 13:18:34 +0300

    finished first tutorial

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+import path from 'path'
 import { app, BrowserWindow } from 'electron'
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
